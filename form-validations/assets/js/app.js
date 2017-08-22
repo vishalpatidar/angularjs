f02376255f395bd2998cc08dfea12b07e04b249a
vishalpatidar <vishal@newput.com>
2017-08-22 18:42:51 +0530

    Validated all fields

--- form-validations/assets/js/app.js
+++ form-validations/assets/js/app.js
@@ -1,3 +1,46 @@
 "use strict";
 
-angular.module("formValidations", []);
+angular.module("formValidations", ['ngFileUpload'])
+    .filter("height", function() {
+        return function(height) {
+            height = parseInt(height) + 100;
+            return height + " cm";
+        };
+    })
+    .filter("salary", function() {
+        return function(salary) {
+            salary = parseInt(salary) * 1000;
+            return salary;
+        };
+    })
+    .controller("validateCtrl", ["$scope", "$http", "$timeout", "$compile", "Upload", function($scope, $http, $timeout, $compile, Upload) {
+
+        $scope.master = {};
+        $scope.user = {height: 50, salary : 50};
+        $scope.user.contact = false;
+
+        $scope.onChange = function (files) {
+          if(files[0] == undefined) return;
+          $scope.fileExt = files[0].name.split(".").pop();
+        }
+
+        $scope.isImage = function(ext) {
+          if(ext) {
+            return ext == "jpg" || ext == "jpeg"|| ext == "gif" || ext=="png"
+          }
+        }
+
+        $scope.update = function(user) {
+            $scope.master = angular.copy(user);
+        };
+
+        $scope.reset = function(form) {
+            if (form) {
+                $scope.user = {height: 50, salary : 50};
+                form.$setPristine();
+                form.$setUntouched();
+            }
+        };
+
+        $scope.reset();
+    }]);
